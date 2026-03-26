03b6a73c3e82e85c8bbbe2bbbc3defd603ec9332
Mohd Anas <mohdanas0x@gmail.com>
2026-03-26 22:46:23 +0530

    js

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,12 +1,26 @@
+// Data Types
+
+// 1.Number 
+// 2.Boollean 
+// 3.String 
+// 4.Undefined 
+// 5.Null 
+// 6.Bigint 
+// 7.Symbol
+
+let x = 25;
+
+console.log(typeof x) ;
+
+
+// conditional statements 
+
 let a = 100 ;
 let b = 20 ;
 
 let c = a - b ;
 
 console.log("difference of a - b is :",c) ;
-
-
-// conditional statements 
 
 if (a > b) {
 
@@ -16,3 +30,92 @@ else  {
 
     console.log("a is less than b") ;
 }
+
+
+// String Methods
+
+let str = "     Hello World     ";
+
+let newStr = str.trim().toUpperCase();      // Method Chaining
+
+console.log(newStr);
+
+console.log(str.length);
+console.log(newStr.length);
+
+console.log(str.slice(8,16));
+
+console.log(str.replace("World","Galaxy"));
+
+console.log(newStr.repeat(3));
+
+console.log(str.indexOf("He"));
+
+
+// ARRAY
+
+let arr = ['Hello',43,23,88.9,0,1];
+
+console.log("Length of array",arr.length);
+console.log(arr[1]);
+console.log(arr[0][1]);
+
+let fruits = ['banana','apple','mango','guava'];
+
+console.log(fruits);
+
+fruits[0] = 'grapes';
+
+console.log(fruits);
+
+
+// Array Methods
+
+// push - add to end
+// unshift - add to start
+// pop - delete from end and returns it
+// shift - delete from start and returns it
+
+console.log(fruits.push('toyota')); 
+console.log(fruits.push('skoda')); 
+
+console.log(fruits.unshift('ferrari')); 
+
+console.log(fruits.pop('ferrari'));
+
+console.log(fruits.shift('ferrari'));
+
+console.log(fruits);
+
+// Concatenation
+
+let plus = arr.concat(fruits); 
+console.log(plus);
+
+console.log(plus.reverse());
+
+// Slice
+
+console.log(fruits.slice(3));
+console.log(fruits.slice(1,3));
+console.log(fruits.slice(-1)); 
+
+// Splice 
+
+console.log("Splice method",plus.splice(0,2));  
+ 
+console.log(plus);
+
+plus.splice( 0,0, "some",'thing');
+
+console.log(plus);
+
+// Sorting
+
+console.log(fruits.sort()); 
+
+
+let nums = [[1,2],[3,4],[5,6]];
+
+console.log(nums);
+
---
+++ Javascript/loop.js
@@ -0,0 +1,110 @@
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// let n = prompt("Write Your Number");
+
+// n = parseInt(n);
+
+// for( let i = n; i<=n*10; i = i+n){
+//     console.log(i);
+// }
+
+// Nested loop with 'loop of'
+
+// let heros = [['ironman','thor','spiderman'],['superman','batman','flash']];
+
+// for (list of heros) {
+//     for(hero of list) {
+//         console.log(hero);
+//     }
+// }
+
+// Object Literals
+
+let student = {
+    name: "chimtu",
+    age: 23,
+    marks: 90.1,
+    city: "New Delhi"
+};
+
+console.log(student);
+
+let post = {
+    username: "@chimtuansari",
+    content: "This is my First Post",
+    like: 150,
+    repost: 5,
+    tags: ["@apnacollege", "@delta"]
+};
+
+console.log(post);
+
+console.log(post["username"]);
+console.log(post.content);
+
+let prop = "repost";
+console.log(post[prop]);
+
+// changing value in object
+
+student.city = 'Mumbai';
+console.log(student);
+
+// adding value
+
+student.gender = "female";
+console.log(student);
+
+delete student.gender;
+console.log(student); 
+
+const classInfo = {
+    aman: {
+        grade: "A+",
+        city: "Delhi"
+    },
+    shradha: {
+        grade: "A",
+        city: "Pune"
+    },
+    karan: {
+        grade: "O",
+        city: "Mumbai"
+    }
+};
+
+console.log(classInfo);
+
+console.log(classInfo.aman);
+console.log(classInfo["karan"]); 
+
+console.log(classInfo.shradha.city);
+console.log(classInfo.aman.city);
+
+classInfo.aman.city = "Gurugram" 
+console.log(classInfo.aman.city);  
+
+
+const classInfo1 = [
+    {
+        name: "aman",
+        grade: "A+",
+        city: "Delhi"
+    },
+    {
+        name: "Shraddha",
+        grade: "A",
+        city: "Pune"
+    },
+    {
+        name: "karan",
+        grade: "O",
+        city: "Mumbai"
+    }
+];
+
+console.log(classInfo1); 
+console.log(classInfo1[1].city); 
+console.log(classInfo1[2].name); 
