b800d7d3bdfe6418b4d0663f0a3b7755053ce095
Mohd Anas <mohdanas0x@gmail.com>
2026-03-26 11:44:36 +0530

    Initial commit

---
+++ Javascript/app.js
@@ -0,0 +1,18 @@
+let a = 100 ;
+let b = 20 ;
+
+let c = a - b ;
+
+console.log("difference of a - b is :",c) ;
+
+
+// conditional statements 
+
+if (a > b) {
+
+    console.log("a is greater than b") ;
+}
+else  {
+
+    console.log("a is less than b") ;
+}
