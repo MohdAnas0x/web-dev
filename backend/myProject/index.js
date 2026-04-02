ada554fcbb07826a712f63f0c186965bf20dce40
Mohd Anas <mohdanas0x@gmail.com>
2026-04-02 15:46:02 +0530

    node js

---
+++ backend/myProject/index.js
@@ -0,0 +1,8 @@
+let giveMeAJoke = require('give-me-a-joke');
+
+// To get a random dad joke
+giveMeAJoke.getRandomDadJoke (function(joke) {
+     console.log(joke);
+});
+
+
---
+++ backend/script.js
@@ -0,0 +1,29 @@
+// let n = 5;
+
+// for (i = 0; i <=n; i++) {
+//     console.log("Hello", i);
+// }
+
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for (i = 2; i < args.length; i++) {
+//     console.log("hello", args[i]);
+// }
+
+// module.exports // require
+
+// const info = require("./fruits");
+
+// console.log(info);
+// console.log(info[1].color);
+ 
+
+//import 
+
+
+import {sum, pi} from "./math.js";
+
+console.log(sum(11,2));
